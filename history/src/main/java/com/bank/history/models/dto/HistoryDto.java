77996ed2c8a83afae30b09751b11a4c2275d1d30
777 <goszakupki.aktiv@gmail.com>
2023-01-17 12:25:36 +0300

    1)Запустил maven clean + install. Ошибок нет, кроме пункта 2. 2) Единственная проблема с HistoryMapperImpl не решена 3) Маппер находится в классе папке модел 4) Добавил аннотации классов и методов по HELP.MD (как понял). 5) Папка exception, классы и методы в них не расписывал, имеют крайне сырой вид, так как будут доделываться в будущих тасках.

--- history/src/main/java/com/bank/history/models/dto/HistoryDto.java
+++ history/src/main/java/com/bank/history/models/dto/HistoryDto.java
@@ -5,7 +5,7 @@ import lombok.Data;
 /**
  * A DTO for the {@link com.bank.history.models.History} entity
  * 7 столбцов, как в entity: 1 столбец - id=primary key
- * Остальные 6 столбцов Long, Nullable
+ * Остальные 6 столбцов Long,  Nullable
  */
 @Data
 public class HistoryDto {
